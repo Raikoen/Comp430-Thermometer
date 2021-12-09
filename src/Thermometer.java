3652fd0705f2838c660ad12604373b556fe0d623
Kyle <cole.kairu@gmail.com>
2021-12-09 12:05:44 -0600

    need to re-evaluate selfTest()

---
+++ src/Thermometer.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Random;
+import java.text.DecimalFormat;
+import java.math.RoundingMode;
+
+
+public class Thermometer extends ThermometerSystem
+{
+  boolean testPassed;
+  boolean feverDetected;
+
+  /*
+    This class is unused. The code we have runs without it...How should we impleeent this?
+    Also, with how things are now, Thermometer does nothing.
+
+     public void selfTest()
+  {
+    //Group effort
+
+    System.out.println("Self-Test beginning now");
+    //Just a placeholder to make sure we can get to this point
+    testPassed=true;
+
+    if(testPassed==true)
+    {
+      System.out.println("Test passed");
+      ThermometerSystem thermometerSystem = new ThermometerSystem();
+      thermometerSystem.mainMenu();
+    }
+
+  }
+   */
+
+
+  }
+
+/*public class Log
+{
+	//Alvaro
+
+		/*
+		float[] logEntries;
+
+		public addTemp(float entry);
+		public deleteTemp(float entry);
+
+		These aren't declared properly, they're placeholders
+
+}
+*/
+
+
+
